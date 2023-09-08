e964aae76c6a00a4a95f3766d88c25a5f16981ea
Paul <fofr@users.noreply.github.com>
2023-09-08 16:55:20 +0100

    Add favicon and og

--- lib/emoji_web.ex
+++ lib/emoji_web.ex
@@ -17,7 +17,7 @@ defmodule EmojiWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico og.webp robots.txt)
 
   def router do
     quote do
