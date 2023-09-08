c8a49efc8a2f07f29efaaac807761c264513e38f
Paul <fofr@users.noreply.github.com>
2023-09-08 15:40:28 +0100

    downcase the prompt

--- lib/emoji_web/live/home_live.ex
+++ lib/emoji_web/live/home_live.ex
@@ -41,7 +41,7 @@ defmodule EmojiWeb.HomeLive do
 
   def handle_event("save", %{"prompt" => prompt}, socket) do
     styled_prompt =
-      (@preprompt <> String.trim_trailing(prompt))
+      (@preprompt <> String.trim_trailing(String.downcase(prompt)))
       |> String.replace("emoji of a a ", "emoji of a ")
       |> String.replace("emoji of a an ", "emoji of an ")
 
