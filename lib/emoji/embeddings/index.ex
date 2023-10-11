fe3b5a9bd43d09f6d194188813e09e5f4ccfebac
Charlie Holtz <charlieholtz@gmail.com>
2023-10-11 14:22:22 -0700

    remove index again

--- lib/emoji/embeddings/index.ex
+++ lib/emoji/embeddings/index.ex
@@ -11,22 +11,22 @@ defmodule Emoji.Embeddings.Index do
   end
 
   def init(_args) do
-    {:ok, full_index} = HNSWLib.Index.new(:l2, 1024, 100_000)
-    {:ok, image_index} = HNSWLib.Index.new(:l2, 1024, 100_000)
+    # {:ok, full_index} = HNSWLib.Index.new(:l2, 1024, 100_000)
+    # {:ok, image_index} = HNSWLib.Index.new(:l2, 1024, 100_000)
 
-    Emoji.Predictions.list_predictions_with_text_embeddings()
-    |> Enum.each(fn prediction ->
-      HNSWLib.Index.add_items(full_index, Nx.from_binary(prediction.embedding, :f32),
-        ids: Nx.tensor([prediction.id])
-      )
-    end)
+    # Emoji.Predictions.list_predictions_with_text_embeddings()
+    # |> Enum.each(fn prediction ->
+    #   HNSWLib.Index.add_items(full_index, Nx.from_binary(prediction.embedding, :f32),
+    #     ids: Nx.tensor([prediction.id])
+    #   )
+    # end)
 
-    Emoji.Predictions.list_predictions_with_image_embeddings()
-    |> Enum.each(fn prediction ->
-      HNSWLib.Index.add_items(image_index, Nx.from_binary(prediction.image_embedding, :f32),
-        ids: Nx.tensor([prediction.id])
-      )
-    end)
+    # Emoji.Predictions.list_predictions_with_image_embeddings()
+    # |> Enum.each(fn prediction ->
+    #   HNSWLib.Index.add_items(image_index, Nx.from_binary(prediction.image_embedding, :f32),
+    #     ids: Nx.tensor([prediction.id])
+    #   )
+    # end)
 
     Logger.info("Index successfully created")
     {:ok, %{full_index: %{}, image_index: %{}}}
